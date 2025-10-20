2cc517d99b393ff017fffbeb394348374e4300b1
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-20 13:35:23 -0600

    Add files via upload
    Función escalar (Devuelve un único valor de cualquier tipo de datos)

---
+++ funciones/CalculoIVA.sql
@@ -0,0 +1,7 @@
+﻿CREATE FUNCTION CalculoIVA (@Price MONEY) RETURNS MONEY
+AS
+BEGIN
+	DECLARE @IVA MONEY       -- DECLARE se utiliza para declarar variables
+  SET @IVA = @Price * 1.16 -- SET se utiliza para Modificar o asignar valores
+  RETURN @IVA              -- RETURN se utiliza para obtener el resultado
+END
