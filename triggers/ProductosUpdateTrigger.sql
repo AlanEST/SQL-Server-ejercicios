3a9b6d8df0366abaca21ebc3c3a5d83768af76ef
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-20 17:29:25 -0600

    Add files via upload
    Primero se creó el campo N_Products en la tabla Employees que guarda el registro del cantidad de productos vendida al generarse una nueva orden (en la tabla Order Details)

---
+++ triggers/ProductosUpdateTrigger.sql
@@ -0,0 +1,12 @@
+﻿ALTER TRIGGER ProductosUpdate  --Nombre del Trigger
+ON [Order Details] --Tabla a la que estará anclado el Trigger
+AFTER INSERT -- Primer se hace el INSERT y después se ejecuta el Trigger
+AS 
+BEGIN 
+  DECLARE @Cantidad SMALLINT, @EmployeeID INT, @Productos INT, @Ordenid INT --Variables a ocupar
+  SELECT @Cantidad=Quantity, @Ordenid =OrderID FROM INSERTED --Ocupamos Quantity y OrderID de los datos insertados
+  SELECT @EmployeeID=EmployeeID  FROM Orders WHERE OrderID=@Ordenid --Extraemos el EmployeeID a partir del OrderID insertado
+  SELECT @Productos=N_Productos FROM Employees WHERE EmployeeID=@EmployeeID --Llamamos al campo N_Products solo para el empleado deseado
+  UPDATE Employees SET N_Productos = @Productos + @Cantidad --Actualizamos N_Products de la tabla Employees, con el registro de la cantidad vendida
+  WHERE EmployeeID=@EmployeeID --solo para el empleado deseado
+END
