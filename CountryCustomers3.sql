be61b286138b3b3c8720a52f5d91acaad170d2ed
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-20 11:01:04 -0600

    Add files via upload
    Función en línea de múltiples sentencias (Son similares a las funciones en línea, el resultado está compuesto por la ejecución de varios "SELECT")

---
+++ CountryCustomers3.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION ClienteporPais (@Country NVARCHAR(15)) 
+RETURNS @TablaPais TABLE (CustomerID NCHAR(5), 
+                        CompanyName NVARCHAR(40), 
+                        ContactName NVARCHAR(30),
+                        Country NVARCHAR(15))
+AS
+BEGIN
+  INSERT INTO @TablaPais
+  SELECT CustomerID, CompanyName, ContactName, Country FROM Customers WHERE Country=@Country
+  RETURN
+END
