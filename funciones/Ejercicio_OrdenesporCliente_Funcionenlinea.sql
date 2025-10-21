4c40469c9bca2e6aa14734d7c421c4636ea264cc
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-20 19:25:07 -0600

    Add files via upload

---
+++ funciones/Ejercicio_OrdenesporCliente_Funcionenlinea.sql
@@ -0,0 +1,4 @@
+﻿CREATE FUNCTION OrdenesCliente (@ClienteId NCHAR(5)) RETURNS TABLE
+RETURN (SELECT OrderID, CustomerID FROM Orders WHERE CustomerID = @ClienteId)
+
+--Su ejecución es SELECT * FROM dbo.OrdenesCliente('HANAR')
