c5b7d48dc9e7c8e1ae19e48915409a7b0eb1906f
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-21 16:09:40 -0600

    Add files via upload

---
+++ Procedimientos Almacenados/CustomerCountry_ProcedimientoAlmacenado1.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE CustomerCountry --Nombre del procedimiento
+AS  --Entre AS y GO se coloca el código a guardar
+SELECT * FROM Customers
+WHERE Country='Mexico'
+GO
+
+--Para ejecutar el procedimiento EXECUTE Nombre del procedimiento, en este caso EXECUTE CustomerCountry 
