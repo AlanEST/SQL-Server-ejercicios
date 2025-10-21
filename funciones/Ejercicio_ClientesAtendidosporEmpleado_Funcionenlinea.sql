fce4a93a293d2fb230adf0bdc54289dd03820309
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-20 19:34:51 -0600

    Add files via upload

---
+++ funciones/Ejercicio_ClientesAtendidosporEmpleado_Funcionenlinea.sql
@@ -0,0 +1,4 @@
+﻿CREATE FUNCTION ClientesAtendidosporEmpleado (@EmpleadoId INT) RETURNS TABLE
+RETURN (SELECT EmployeeID, CustomerID FROM Orders WHERE EmployeeID = @EmpleadoId)
+
+--Su ejecución es SELECT * FROM dbo.ClientesAtendidosporEmpleado(3)
