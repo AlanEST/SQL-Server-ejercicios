c401a368046c5a523bf7608f27c5a0373d8e5250
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-20 19:33:57 -0600

    Delete funciones/Ejercicio_ClientesAtendidosporEmpleado.sql

--- funciones/Ejercicio_ClientesAtendidosporEmpleado.sql
+++
@@ -1,4 +0,0 @@
-﻿CREATE FUNCTION ClientesAtendidosporEmpleado (@EmpleadoId INT) RETURNS TABLE
-RETURN (SELECT EmployeeID, CustomerID FROM Orders WHERE EmployeeID = @EmpleadoId)
-
---Su ejecución es SELECT * FROM dbo.ClientesAtendidosporEmpleado(3)
