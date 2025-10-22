4cb53ffc8af3ca38a65feb4a3cb194fc96937f6c
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-21 18:57:38 -0600

    Add files via upload

---
+++ Procedimientos Almacenados/EjecucionProcedimientoAlmacenado2.sql
@@ -0,0 +1,5 @@
+﻿DECLARE @back NCHAR(5)  --se declara una variable que obtenga el valor de retorno del procedimiento (mismo tipo de dato y longitud)
+EXECUTE InsertUpdate_Customers 'ABABA', 'Puzzle', @back OUTPUT   -- EXECUTE Nombre del procedimiento | valores a insertar | variable recién
+--Al ejecutar debe aparecer uno de los mensajes declarados en el procedimiento
+SELECT @back -- Para conocer el valor de la variable de retorno 
+
