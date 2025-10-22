448e55dd7272353bffde5ebdffa111a8dcb6c431
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-21 19:02:05 -0600

    Add files via upload

---
+++ Procedimientos Almacenados/InsertUpdate_Customers_ProcedimientoAlmacenado2.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE InsertUpdate_Customers @CustomerID NCHAR(5), @CompanyName NVARCHAR(40), @ReturnID NCHAR(5) OUTPUT
+AS 
+BEGIN
+INSERT INTO Customers(CustomerID, CompanyName) VALUES (@CustomerID, @CompanyName)
+IF (@@ERROR <>0)
+  BEGIN
+    UPDATE Customers
+    SET Customers.CustomerID=@CustomerID, Customers.CompanyName=@CompanyName
+    WHERE Customers.CustomerID=@CustomerID
+    PRINT('Se realizó un UPDATE')
+  END
+ELSE 
+  BEGIN
+    PRINT('Se realizó un INSERT')
+  END
+SELECT @ReturnID=CustomerID FROM Customers c WHERE c.CustomerID = @CustomerID --Llenado de la variable de retorno con el CustomerID que se insertó o actualizó (el que se le pasó)
+END
+
+-- @@ERROR es una variable del sistema en SQL Server que devuelve el número del último error ocurrido;
+-- devuelve cero si no hubo error. 
+-- Es útil para detectar fallos inmediatamente después de ejecutar una declaración SQL.
