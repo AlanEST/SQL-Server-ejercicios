d70a31ff27314449530aeb9f8c416434f50b738d
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-10-24 17:58:36 -0600

    Add files via upload
    En este caso, del Esquema y Tabla HumanResources.EmployeeDepartmentHistory del Campo EndDate se filtraron todos aquellos donde no se tuviera una fecha de término (considerando solo el departamento actual del empleado y no sus movimientos internos)

---
+++ Procedimientos Almacenados/Ejercicio_EmpledosPuestoyDepartamento_PAsinparametros.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE EmpleadosPuestoyDepartamento --Nombre del procedimiento
+AS  --Entre AS y GO se coloca el código a guardar
+SELECT e.BusinessEntityID AS IDEmpleado, p.FirstName AS Nombre, p.MiddleName AS ApellidoMaterno ,p.LastName AS ApellidoPaterno,
+e.JobTitle AS Puesto, d.Name AS Departamento
+FROM HumanResources.Employee e 
+JOIN Person.Person p ON e.BusinessEntityID = p.BusinessEntityID
+JOIN HumanResources.EmployeeDepartmentHistory edh ON e.BusinessEntityID = edh.BusinessEntityID
+JOIN HumanResources.Department d ON edh.DepartmentID = d.DepartmentID
+WHERE edh.EndDate IS NULL
+ORDER BY e.BusinessEntityID ASC
+GO
+
+-- La ejecución en este caso es: EXECUTE EmpleadosPuestoyDepartamento
