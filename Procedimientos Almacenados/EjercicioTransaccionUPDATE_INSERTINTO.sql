2b5c8915f71b2f7057142e25b4053a91914ab476
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-04 10:55:03 -0600

    Add files via upload

---
+++ Procedimientos Almacenados/EjercicioTransaccionUPDATE_INSERTINTO.sql
@@ -0,0 +1,18 @@
+﻿BEGIN TRANSACTION 
+  UPDATE HumanResources.Employee SET NationalIDNumber = 987654321 WHERE BusinessEntityID = 291
+  INSERT INTO HumanResources.Employee(BusinessEntityID, NationalIDNumber, LoginID, JobTitle, BirthDate, MaritalStatus,
+  Gender, HireDate) 
+  VALUES (293, 888888888, 'adventure-works\Catherin2','Desempleado2', '1994-12-10', 'S', 'F','2021-10-10')
+  IF (@@Error<>0)
+    BEGIN
+      ROLLBACK
+      PRINT('ERROR')
+    END
+  ELSE
+    BEGIN
+      COMMIT
+      PRINT('BIEN')
+    END
+
+-- Tener cuidado con las llaves foráneas (recuerda que las tablas se vinculan entre llaves primaria-foránea)
+-- Introducir los datos en el formato adecuado para cada tipo de dato
