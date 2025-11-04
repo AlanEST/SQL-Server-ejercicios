2920bc89fb9000d3c9ba7b72d9cedc381ca059de
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-04 13:36:47 -0600

    Add files via upload

---
+++ Consultas/InstruccionDISTINCT.sql
@@ -0,0 +1,8 @@
+﻿SELECT DISTINCT od.ProductID, p.ProductName  FROM [Order Details] od
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+WHERE o.OrderDate >= '1996-07-04'AND o.OrderDate <= '1996-07-05'
+ORDER BY od.ProductID
+
+-- Sin la instrucción DISINCT se consultan los productos uno por uno, instroducciendo 
+-- esta instrucción se eliminan los duplicados
