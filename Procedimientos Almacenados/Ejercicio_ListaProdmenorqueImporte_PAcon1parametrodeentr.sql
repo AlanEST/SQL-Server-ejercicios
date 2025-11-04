a85819f1c066ad953640b42844d108757202b1e9
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-03 20:43:34 -0600

    Add files via upload

---
+++ Procedimientos Almacenados/Ejercicio_ListaProdmenorqueImporte_PAcon1parametrodeentr.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE ListadeProductosmenorqueImporte
+@Limiteprecio MONEY
+AS 
+BEGIN
+SELECT p.ProductID, p.Name, p.ProductNumber, p.ListPrice
+FROM Production.Product p WHERE p.ListPrice> 0 AND p.ListPrice < @Limiteprecio AND p.SellEndDate IS NULL
+END
+
+-- La ejecuación en este caso es: EXECUTE AdventureWorks2008.dbo.ListadeProductosmenorqueImporte 40
+-- Para la ejecución de este PA solo se ocupó el nombre del procedimiento y el valor del parámetro de entrada
