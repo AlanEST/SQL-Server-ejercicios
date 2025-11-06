05c82c7334ac8729f9067489e6ce4a90d065858c
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-06 16:01:00 -0600

    Add files via upload

---
+++ Consultas/InstruccionINTO.sql
@@ -0,0 +1,8 @@
+﻿IF OBJECT_ID('CustomerCopy') IS NOT NULL  --Primero verificamos si la tabla ya existe en la BD
+BEGIN
+  DROP TABLE CustomerCopy --Borramos la tabla
+END
+
+SELECT CustomerID, CompanyName  --Campos a guardar en la nueva tabla
+INTO CustomerCopy
+FROM Customers
