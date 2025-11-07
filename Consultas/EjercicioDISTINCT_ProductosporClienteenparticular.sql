183aebed3b0d29e0493c62df7b1f3d57489dc151
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-06 22:57:05 -0600

    Add files via upload

---
+++ Consultas/EjercicioDISTINCT_ProductosporClienteenparticular.sql
@@ -0,0 +1,11 @@
+﻿-- Lista de productos ordenados por un cliente en particular
+SELECT DISTINCT od.ProductID, p.ProductName  FROM Orders o
+INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+WHERE o.CustomerID = 'ALFKI'
+ORDER BY p.ProductName
+
+--Si quitamos el DISTINCT se genera una lista del Total de Productos que pidió 'ANATR' en en Total de sus Órdenes
+-- en este caso hizo 4 Órdenes con un Total de 10 Productos, de los cuales, todos son distintos
+-- Si consideramos el Cliente 'ALFKI', vemos cómo éste solicitó un total de 12 productos en todas sus órdenes, pero 
+-- 11 de ellos son distintos
