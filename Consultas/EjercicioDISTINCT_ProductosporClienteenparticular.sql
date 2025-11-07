2ba487f8f6d6fcbf4c91f2b4639264697d575d58
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-06 22:56:36 -0600

    Delete Consultas/EjercicioDISTINCT_ProductosporClienteenparticular.sql

--- Consultas/EjercicioDISTINCT_ProductosporClienteenparticular.sql
+++
@@ -1,6 +0,0 @@
-﻿-- Lista de productos ordenados por un cliente en particular
-SELECT DISTINCT od.ProductID, p.ProductName  FROM Orders o
-INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
-INNER JOIN Products p ON od.ProductID = p.ProductID
-WHERE o.CustomerID = 'ANATR'
-ORDER BY p.ProductName
