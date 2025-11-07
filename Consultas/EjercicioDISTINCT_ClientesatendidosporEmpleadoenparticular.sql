b4d45441a67953108b56459c79c2f841921efcea
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-06 18:44:36 -0600

    Add files via upload

---
+++ Consultas/EjercicioDISTINCT_ClientesatendidosporEmpleadoenparticular.sql
@@ -0,0 +1,4 @@
+﻿-- Listado de clientes con el nombre de su compañía que atendió un empleado en particular
+SELECT DISTINCT o.CustomerID, c.CompanyName FROM Orders o
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE o.EmployeeID = 7
