07be4449adf15d6035595353a8e7d7931222596a
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-11 11:17:38 -0600

    Add files via upload

---
+++ Consultas/Ejercicio_GROUPBYORDERBY_GastoTotalporCliente.sql
@@ -0,0 +1,6 @@
+﻿-- Consumo Total por Cliente (con su Nombre de Compañía) ordenado por el Id del Cliente
+SELECT c.CustomerID, c.CompanyName, sum(od.UnitPrice * od.Quantity * (1-od.Discount)) TotalCompra  FROM Customers c
+INNER JOIN Orders o ON c.CustomerID = o.CustomerID
+INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+GROUP BY c.CustomerID, c.CompanyName
+ORDER BY c.CustomerID
