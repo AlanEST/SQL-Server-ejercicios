ffc32e1b04951560374a55aeddf0bbb15ee96ace
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-10 20:50:54 -0600

    Add files via upload

---
+++ Consultas/Ejercicio_Subconsulta_EmpleadosquepertenecenaRegionNorte.sql
@@ -0,0 +1,19 @@
+﻿SELECT DISTINCT e.EmployeeID, RegionDescription  FROM Employees e
+INNER JOIN EmployeeTerritories et ON e.EmployeeID = et.EmployeeID
+INNER JOIN Territories t ON et.TerritoryID = t.TerritoryID
+INNER JOIN Region r ON t.RegionID = r.RegionID
+WHERE 'Northern' IN (SELECT RegionDescription  FROM Region r WHERE t.RegionID = r.RegionID)
+
+-- Es necesario el INNER JOIN con la tabla Region debido a que se traé el campo RegionDescription
+
+SELECT DISTINCT e.EmployeeID, RegionDescription  FROM Employees e
+INNER JOIN EmployeeTerritories et ON e.EmployeeID = et.EmployeeID
+INNER JOIN Territories t ON et.TerritoryID = t.TerritoryID
+INNER JOIN Region r ON t.RegionID = r.RegionID
+WHERE r.RegionDescription = 'Northern'
+
+SELECT DISTINCT e.EmployeeID, RegionDescription  FROM Employees e
+INNER JOIN EmployeeTerritories et ON e.EmployeeID = et.EmployeeID
+INNER JOIN Territories t ON et.TerritoryID = t.TerritoryID
+INNER JOIN Region r ON t.RegionID = r.RegionID
+WHERE RegionDescription IN (SELECT RegionDescription  FROM Region r WHERE RegionDescription = 'Northern')
