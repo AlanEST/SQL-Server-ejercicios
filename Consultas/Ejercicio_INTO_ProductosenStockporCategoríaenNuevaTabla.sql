f6c64eae2161fef693bb0f294fcd3d945f34eef4
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-11 11:25:19 -0600

    Add files via upload

---
+++ Consultas/Ejercicio_INTO_ProductosenStockporCategoríaenNuevaTabla.sql
@@ -0,0 +1,11 @@
+﻿IF OBJECT_ID('ProdutosenStock') IS NOT NULL  --Primero verificamos si la tabla ya existe en la BD
+BEGIN
+  DROP TABLE ProductosenStock --Borramos la tabla
+END
+
+SELECT c.CategoryID, c.CategoryName, SUM(p.UnitsInStock) UnitsInStock  --Campos a guardar en la nueva tabla
+INTO ProductosenStock
+FROM Products p
+INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+GROUP BY c.CategoryName, c.CategoryID
+ORDER BY c.CategoryID
