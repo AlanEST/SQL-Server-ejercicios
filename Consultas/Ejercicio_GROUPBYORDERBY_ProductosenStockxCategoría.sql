430fb6dab475c02ebd33680b9c44bcf6368b170f
AlanEST <113927155+AlanEST@users.noreply.github.com>
2025-11-11 11:27:28 -0600

    Add files via upload

---
+++ Consultas/Ejercicio_GROUPBYORDERBY_ProductosenStockxCategoría.sql
@@ -0,0 +1,5 @@
+﻿-- Total de productos que existen en stock por Categoría ordenados por el Id de Categoría
+SELECT c.CategoryID, c.CategoryName, SUM(p.UnitsInStock) UnitsInStock FROM Products p
+INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+GROUP BY c.CategoryName, c.CategoryID
+ORDER BY c.CategoryID
